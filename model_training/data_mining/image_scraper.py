5c8ad69631e203c7b0bc67c62717fd9422207902
MingCong, Zhou <mzhou272@uwo.ca>
2020-08-22 13:59:06 -0400

    Wrote a web scraper for image collection

---
+++ model_training/data_mining/image_scraper.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import requests
+import random
+import os
+from PIL import Image
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+
+labels = ['plant_name', 'link']
+
+df = pd.read_csv("mushroom.csv", names=labels)
+
+print(df.head())
+
+user_agent = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+]
+
+session = requests.session()
+
+for index, row in df.iterrows():
+	
+	print(row['plant_name'], row['link'])
+	
+	try: 
+		response = session.get(row['link'], headers={'User-Agent': random.choice(user_agent)})
+		parsed_html = BeautifulSoup(response.text, 'html.parser')
+
+		images = parsed_html.find_all(alt="Picture")
+		
+		path = "./image/" + row['plant_name'] + "/"
+		os.mkdir(path)
+
+		for img in images:
+
+			image_url = "http://www.mushroom.world/" + img['src'][3:].replace("-thumb", "")
+			image_name = image_url.split("/")[-1]
+
+			try:
+				img = Image.open(requests.get(image_url, stream = True).raw)
+
+				if img.mode in ("RGBA", "P"): 
+					img = img.convert("RGB")
+
+				img.save(path + image_name, quality=100, subsampling=0)
+			except:
+				pass
+	except:
+		pass
+
